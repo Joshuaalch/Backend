eb2f81446e8b89befbb7b820b567db6ac76cb669
MaizethC <119091973+MaizethC@users.noreply.github.com>
2025-01-10 18:20:58 -0600

    Módulo de consultas
    Creación del backend del modulo de consultas

---
+++ controllers/controllerConsultation.js
@@ -0,0 +1,72 @@
+const ConsultationData = require("../data/consultationData");
+
+class ControllerConsultation {
+
+  // Crear consluta
+  async createConsultation(data) {
+    try {
+      const result = await ConsultationData.createConsultation(data); // Método estático
+      return { success: true, id: result };
+    } catch (error) {
+      console.error("Error al crear consulta:", error.message);
+      throw error;
+    }
+  }
+
+  // Obtener todas las consultas
+  async getAllConsultation() {
+    try {
+      const consultation = await ConsultationData.getAllConsultation(); // Método estático, devuelve todos los usuarios (pacientes)
+      return consultation;
+    } catch (error) {
+      console.error("Error al obtener pacientes:", error.message);
+      throw error;
+    }
+  }
+
+  // Obtener un usuario por cedula
+  async getUserByCedula(cedula) {
+    try {
+      const user = await PatientData.getUserByCedula(cedula); // Método estático
+
+      if (!user) {
+        throw new Error(No se encontró paciente con la cédula ${cedula});
+      }
+    
+      return user;
+    } catch (error) {
+      console.error("Error al obtener usuarios:", error.message);
+      throw error;
+    }
+  }
+
+  // Eliminar usuario por cédula
+  async deleteUserByCedula(cedula) {
+    try {
+      const result = await PatientData.deleteUser(cedula); // Método estático
+      if (!result) {
+        throw new Error(No se encontró paciente con la cédula ${cedula});
+      }
+      return { success: true, message: "Usuario eliminado con éxito" };
+    } catch (error) {
+      console.error("Error al eliminar usuario:", error.message);
+      throw error;
+    }
+  }
+
+  // Actualizar usuario por cédula
+  async updateUserByCedula(data) {
+    try {
+      const result = await PatientData.updateUser(data); // Método estático
+      if (!result) {
+        throw new Error(No se encontró un usuario con la cédula ${data.id_cedula});
+      }
+      return { success: true, message: "Usuario actualizado con éxito" };
+    } catch (error) {
+      console.error("Error al actualizar usuario:", error.message);
+      throw error;
+    }
+  }
+}
+
+module.exports = ControllerConsultation;
---
+++ routes/consultationRouter.js
@@ -0,0 +1,120 @@
+const ControllerConsultation = require("../controllers/controllerConsultation");
+const validateBody = require("../middlewares/validateBody");
+
+const consultationRouter = (app) => {
+  let response = {
+    data: "message",
+    code: "code",
+  };
+  
+  //obtener paciente por cedula/ ruta api
+  app.route("/consultation/:id_consulta")
+  .get(async (req, res) => {
+    try {
+      const { cedula } = req.params; // Obtiene la cédula de la URL
+      const controller = new ControllerPatient();
+      const user = await controller.getUserByCedula(cedula);
+
+      if (user) {
+        response.data = user;
+        response.code = "200";
+      } else {
+        response.data = "Usuario no encontrado";
+        response.code = "404";
+      }
+    } catch (error) {
+      response.data = error.message;
+      response.code = "500";
+    }
+    res.send(response);
+  })
+
+ //eliminar paciente por cedula
+  app.route("/patient/:cedula")
+
+  .delete(async (req, res) => {
+    try {
+      const controller = new ControllerPatient();
+      const { cedula } = req.params;
+
+      if(!cedula){
+          response.data = "La cédula es obligatoria";
+          response.code = "400";
+          return res.send(response);
+      }
+
+      const result = await controller.deleteUserByCedula(cedula);
+      if (result.success) {
+        response.data = result.message;
+        response.code = "200";
+      } else {
+        response.data = "El paciente No fue eliminado";
+        response.code = "400";
+      }
+    } catch (error) {
+      response.data = error.message;
+      response.code = "500";
+    }
+    res.send(response);
+  });
+
+ //crear las citas
+  app.route("/consultation")
+  .post(async (req, res) => {
+    const response = {};
+    try {
+      const controller = new ControllerConsultation();
+      const result = await controller.createConsultation(req.body);
+
+      if (result.success) {
+        response.data = "La consulta fue creada correctamente";
+        response.code = "200";
+      } else {
+        response.data = "La consulta No fue creado";
+        response.code = "400";
+      }
+    } catch (error) {
+      response.data = error.message;
+      response.code = "500";
+    }
+    res.send(response);
+  })
+
+
+    // Obtener todos los pacientes
+    .get(async (req, res) => {
+      try {
+        const controller = new ControllerConsultation();
+        const consultation = await controller.getAllConsultation();
+        response.data = consultation;
+        response.code = "200";
+      } catch (error) {
+        response.data = error.message;
+        response.code = "500";
+      }
+     
+      res.send(response);
+    })
+    
+    // Actualizar paciente
+    .patch(async (req, res) => {
+      try {
+        const controller = new ControllerPatient();
+        const result = await controller.updateUserByCedula(req.body);
+        if (result.success) {
+          response.data = result.message;
+          response.code = "200";
+        } else {
+          response.data = "El Usuario No fue actualizado";
+          response.code = "400";
+        }
+      } catch (error) {
+        response.data = error.message;
+        response.code = "500";
+      }
+      res.send(response);
+    });
+
+};
+
+module.exports = consultationRouter;
